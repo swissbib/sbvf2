f7d68fd26839922b637845230297cd0916ae84b5
Guenter Hipler <guenter.hipler@unibas.ch>
2013-01-02 16:24:09 +0100

    Creation of a Swissbib/RecordDriver/SbSolrMarc which derives from VuFind/RecordDriver/SolrMarc Pros: we can overwrite only the desired methods of the RecordDriver's in the VuFind namespace
    possible Cons:
    topics for more consideration:
    - performance of PHP in case methods are overwritten? -> Snowflake?
    - there is another configuration in VuFind\config\module.config.php for VuFind\RecordDriver\SolrMarc
    (recorddriver_tabs)
    enhancement of the swissbib module configuration necessary?

---
+++ module/Swissbib/src/Swissbib/RecordDriver/SbSolrMarc.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * swissbib / VuFind swissbib enhancements for MARC records in Solr
+ *
+ * PHP version 5
+ *
+ * Copyright (C) project swissbib, University Library Basel, Switzerland
+ * http://www.swissbib.org  / http://www.swissbib.ch / http://www.ub.unibas.ch
+ *
+ * Date: 1/2/13
+ * Time: 4:09 PM
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category swissbib_VuFind2
+ * @package  RecordDriver
+ * @author   Guenter Hipler  <guenter.hipler@unibas.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link    http://www.swissbib.org
+ */
+
+
+namespace Swissbib\RecordDriver;
+
+use VuFind\RecordDriver\SolrMarc as VFSolrMarc;
+
+
+
+/**
+ * enhancement for swissbib MARC records in Solr.
+ *
+ * @category swissbib_VuFind2
+ * @package  RecordDrivers
+ * @author   Guenter Hipler  <guenter.hipler@unibas.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.swissbib.org
+ */
+class SbSolrMarc extends VFSolrMarc
+{
+
+    //public function setRawData($data)
+    //{
+    //    //only for test purposes within this type to see if the type is correct instantiated
+        // Call the parent's set method...
+    //    parent::setRawData($data);
+    //}
+
+    //now you can start to override the desired functions or to create new functions used by the view-scrips
+}
