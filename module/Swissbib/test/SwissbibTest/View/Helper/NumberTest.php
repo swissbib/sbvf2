5ffc1a70ac7cdcafa977d511e512389473038db3
Fabian Erni <ferni@snowflake.ch>
2013-02-07 15:49:14 +0100

    * Improved ci config * Added unittest and config

---
+++ module/Swissbib/test/SwissbibTest/View/Helper/NumberTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace SwissbibTest;
+
+use Swissbib\View\Helper\Number;
+
+
+class NumberTest extends \PHPUnit_Framework_TestCase {
+
+	public function testInvoke() {
+		$number		= new Number();
+		$input		= 123456;
+		$expected	= '123\'456';
+		$output		= $number($input);
+
+		$this->assertEquals($expected, $output);
+	}
+
+}
