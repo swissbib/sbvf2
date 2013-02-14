ba866f3760275f908514da9bf5e8b9027e9b8a23
Fabian Erni <ferni@snowflake.ch>
2013-02-14 15:25:45 +0100

    * Added basic test case for swissbib xserver

---
+++ module/Swissbib/src/Swissbib/Controller/XserverController.php
@@ -0,0 +1,32 @@
+<?php
+namespace Swissbib\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Swissbib\XServer\Client as XServerClient;
+
+use Swissbib\XServer\Exception\Exception as xException;
+
+class XserverController extends AbstractActionController {
+
+	public function testAction() {
+		$client	= new XServerClient('http://alephtest.unibas.ch:8991/X');
+		$client->setCredentials('BWWW49826', 'VUFIND');
+
+		try {
+			$userKey	= $client->getUserID();
+		} catch(xException $e) {
+			die($e->getMessage());
+		}
+
+		$view = new ViewModel(array(
+			'userkey' => $userKey,
+		));
+
+		// Disable layouts; `MvcEvent` will use this View Model instead
+		$view->setTerminal(true);
+
+		return $view;
+	}
+
+}
