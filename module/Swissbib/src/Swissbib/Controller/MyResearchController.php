72838a0fa4b3e0b2a9a9433f0b821c5a286ba12d
Fabian Erni <ferni@snowflake.ch>
2013-02-20 18:25:41 +0100

    * Cleanup profile code * Added getPhotoCopies() functionality to swissbib aleph driver * Added new menu item photocopies in account

--- module/Swissbib/src/Swissbib/Controller/MyResearchController.php
+++ module/Swissbib/src/Swissbib/Controller/MyResearchController.php
@@ -2,10 +2,26 @@
 namespace Swissbib\Controller;
 
 use VuFind\Controller\MyResearchController as VFMyResearchController;
-use Zend\View\Model\ViewModel;
+use Swissbib\VuFind\ILS\Driver\Aleph;
 
 
 class MyResearchController extends VFMyResearchController {
+
+
+	public function photocopiesAction() {
+		// Stop now if the user does not have valid catalog credentials available:
+		if( !is_array($patron = $this->catalogLogin()) ) {
+			return $patron;
+		}
+
+		/** @var Aleph $catalog  */
+		$catalog = $this->getILS();
+
+		// Get photo copies details:
+		$photoCopies = $catalog->getPhotocopies($patron);
+
+		return $this->createViewModel(array('photoCopies' => $photoCopies));
+	}
 
 	/**
 	 * Get location parameter from route
@@ -30,5 +46,6 @@ class MyResearchController extends VFMyResearchController {
 
 		return $viewModel;
 	}
+
 
 }
