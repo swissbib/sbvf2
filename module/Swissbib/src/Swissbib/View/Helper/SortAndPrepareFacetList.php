9d0e87ef6a3399ccd7d8df37a7ebbc0f8d0ef15d
Kay Stenschke <kstenschke@snowflake.ch>
2013-03-05 18:54:21 +0100

    * exploration result (temporary implementation for conceptional purpose) - store/fetch local user data

--- module/Swissbib/src/Swissbib/View/Helper/SortAndPrepareFacetList.php
+++ module/Swissbib/src/Swissbib/View/Helper/SortAndPrepareFacetList.php
@@ -14,24 +14,23 @@ class SortAndPrepareFacetList extends AbstractHelper {
 	/**
 	 * Sort and extend facet list
 	 *
-	 * @param	Results		$results
-	 * @param	String		$field
-	 * @param	Array		$list
-	 * @param	String		$searchRoute
-     * @return array
+	 * @param	Results		$results        VuFind\Search\Solr\Results
+	 * @param	String		$field          Facet group ID, e.g. 'navSubidsbb'
+	 * @param	Array		$list           Contained items of the facet group
+	 * @param	String		$searchRoute    e.g. 'search-results'
+     * @return  Array
      */
 	public function __invoke(Results $results, $field, array $list, $searchRoute) {
 		$facets = array();
 		    // Avoid limit on URL
 //		$results->getParams()->setLimit($results->getOptions()->getDefaultLimit());
 		$urlHelper	= $this->getView()->plugin('url');
-		$baseRout	= $urlHelper($searchRoute);
+		$baseRoute	= $urlHelper($searchRoute);
 
-		foreach($list as $facet) {
-			$facet['url']	= $baseRout . $results->getUrlQuery()->addFacet($field, $facet['value']);
-			$facets[$facet['displayText']] = $facet;
+		foreach($list as $facetItem) {
+			$facetItem['url']	= $baseRoute . $results->getUrlQuery()->addFacet($field, $facetItem['value']);
+			$facets[$facetItem['displayText']] = $facetItem;
 		}
-//		ksort($facets, SORT_NATURAL);
 
 		return array_values($facets);
 	}
