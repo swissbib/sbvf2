4e9fa2c4e848666ba5a711bcd518ed6da69938bd
Fabian Erni <ferni@snowflake.ch>
2013-04-17 16:01:16 +0200

    * Unit tests for physical descriptions view helper

---
+++ module/Swissbib/test/SwissbibTest/View/Helper/PhysicalDescriptionsTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace SwissbibTest\View\Helper;
+
+use Swissbib\View\Helper\PhysicalDescriptions;
+
+/**
+ * [Description]
+ *
+ */
+class PhysicalDescriptionsTest extends \PHPUnit_Framework_TestCase {
+
+	public function testEmpty() {
+		$desc	= new PhysicalDescriptions();
+		$data	= array();
+
+		$result	= $desc($data);
+
+		$this->assertInternalType('string', $result);
+		$this->assertEmpty($result);
+	}
+
+
+	public function testNormal() {
+		$desc	= new PhysicalDescriptions();
+		$data	= array(
+			array(
+				'extent' => array(
+					'a',
+					'b'
+				),
+				'details' => 'c',
+				'unknown' => 'x'
+			)
+		);
+
+		$result	= $desc($data);
+
+		$this->assertInternalType('string', $result);
+		$this->assertEquals('a, b, c', $result);
+	}
+
+}
+
+?>
