04795d5b6187ac73aa72db369b9fd3cb6c588252
Fabian Erni <ferni@snowflake.ch>
2013-04-17 15:50:44 +0200

    * Implemented physicalDescriptions ViewHelper as an example

---
+++ module/Swissbib/src/Swissbib/View/Helper/PhysicalDescriptions.php
@@ -0,0 +1,37 @@
+<?php
+namespace Swissbib\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Format physical descriptions data array
+ *
+ */
+class PhysicalDescriptions extends AbstractHelper {
+
+	/**
+	 * Format integer with thousand separator
+	 *
+	 * @param	Array		$physicalDescriptions
+	 * @return	String
+	 */
+	public function __invoke(array $physicalDescriptions) {
+		$infos	= array();
+		$types	= array('extent', 'details', 'dimensions', 'company', 'material_multiple');
+
+		foreach($physicalDescriptions as $physicalDescription) {
+			foreach($types as $type) {
+				if( isset($physicalDescription[$type]) ) {
+					if( is_array($physicalDescription[$type]) ) {
+						$infos	= array_merge($infos, $physicalDescription[$type]);
+					} else {
+						$infos[]= $physicalDescription[$type];
+					}
+				}
+			}
+		}
+
+		return implode(', ', $infos);
+	}
+
+}
