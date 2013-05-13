50e86c97e1064e3bc746bc99b2749b3363fb8072
Oliver Schihin <oliver.schihin@unibas.ch>
2013-05-13 15:48:42 +0200

    title display of subject vocabularies

--- module/Swissbib/src/Swissbib/View/Helper/SubjectVocabularies.php
+++ module/Swissbib/src/Swissbib/View/Helper/SubjectVocabularies.php
@@ -18,6 +18,32 @@ class SubjectVocabularies extends AbstractHelper
 	 */
 	public function __invoke(array $subjectVocabularies)
 	{
-		return '$subjectVocabularies view helper';
+        foreach ($subjectVocabularies as $vocab => $subjectVocabulary) {
+            switch($vocab) {
+                case 'gnd':
+                    $gndtitle = '<h4>gnd</h4>';
+                    foreach ($subjectVocabulary as $fields) {
+                        foreach ($fields as $field) {
+                            //if (!is_numeric($field->getKey()));
+                        };
+                    }
+                    break;
+                case 'lcsh':
+                    $lcshtitle = '<h4>lcsh</h4>';
+                    break;
+                case 'mesh':
+                    $meshtitle = '<h4>MESH</h4>';
+                    break;
+                // to be continued
+                case 'bisacsh':
+                    $bisacshtitle = '<h4>bisacsh</h4>';
+                    break;
+                case 'ids zbz':
+                    $idszbztitle = '<h4>IDS ZB Z&uuml;rich</h4>';
+                    break;
+            }
+            continue;
+        }
+        return $gndtitle . $lcshtitle . $meshtitle . $bisacshtitle . $idszbztitle;
 	}
 }
