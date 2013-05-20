e581486b565cd6cf7143f9debc09f291bc0da483
Guenter Hipler <guenter.hipler@unibas.ch>
2013-05-20 15:47:26 +0200

    more cleanup in javascripts

--- themes/swissbibsingle/theme.config.php
+++ themes/swissbibsingle/theme.config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
     'extends' => 'swissbib',
-    'js' => array(
-        'sb.extensions.singletarget.js',
+    //'js' => array(
+    //    'sb.extensions.singletarget.js',
         //this stuff is very dedicated to the current swissbib TouchPoint component
         //main reason for all this trash: TouchPoint is really weak when it comes to asynch requests
         // I'm pretty sure we can throw away at least 99% of the code
         // but: a little bit later.....
-    ),
+    //),
 );
