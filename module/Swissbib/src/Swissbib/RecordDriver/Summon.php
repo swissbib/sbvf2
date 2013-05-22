05e047af231281075d6af1a31665bd9b92afed6e
Kay Stenschke <kstenschke@snowflake.ch>
2013-05-22 18:25:52 +0200

    fixed fatal error in summon single view (temp. solution) style fix: summon result table width

--- module/Swissbib/src/Swissbib/RecordDriver/Summon.php
+++ module/Swissbib/src/Swissbib/RecordDriver/Summon.php
@@ -92,4 +92,12 @@ class Summon extends VuFindSummon
 		return 1 === intval($this->getField('hasFullText'));
 	}
 
+	/**
+	 * @todo	implement
+	 * @return	array
+	 */
+	public function getAllSubjectVocabularies() {
+		return array();
+	}
+
 }
