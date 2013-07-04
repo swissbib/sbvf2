bbb81147f6acb5c16f45730aa08fe372aa06b0cf
Fabian Erni <ferni@snowflake.ch>
2013-07-04 14:33:06 +0200

    * Added add and delete feature for favorite flags in record holding

--- themes/swissbib/js/swissbib/HoldingFavorites.js
+++ themes/swissbib/js/swissbib/HoldingFavorites.js
@@ -1,46 +1,120 @@
+/**
+ * Manage favorite actions on record holding tab
+ *
+ */
 swissbib.HoldingFavorites = {
 
+	/**
+	 * @var {String}
+	 */
+	baseUrl: '/MyResearch/FavoriteInstitutions',
+
+
+
+	/**
+	 * Initialize record
+	 *
+	 */
 	initRecord: function() {
-		var that = this,
+		this.baseUrl = window.path + this.baseUrl;
+
+		this.setupClickHandlers();
+	},
+
+
+
+	/**
+	 * Setup click handlers for add and remove of favorite institutions
+	 *
+	 */
+	setupClickHandlers: function() {
+		var that = this, allMiniActions, notFavorised,
 			favoriteInstitutionCodes = this.getFavoriteInstitutionCodes();
+
+		this.baseUrl = window.path + this.baseUrl;
 
 		$.each(favoriteInstitutionCodes, function(index, institutionCode){
 			$('.miniactions-' + institutionCode).find('.institutionFavorite')
 					.addClass('miniaction_favorite_remove')
-					.click($.proxy(that.removeFromFavorite, that))
+					.click($.proxy(that.onRemoveFavoriteIconClick, that))
 					.data('favorised', true);
 		});
 
-		var allMiniActions = $('#tab-holdings').find('.miniactions');
-		var notFavorised = $.grep(allMiniActions, function(node, index){
+		allMiniActions = $('#tab-holdings').find('.miniactions');
+		notFavorised = $.grep(allMiniActions, function(node, index){
 			return $(node).find('.institutionFavorite').data('favorised') !== true;
 		});
 
 		$(notFavorised).find('.institutionFavorite')
 				.addClass('miniaction_favorite_add')
-				.click($.proxy(this.addToFavorite, this));
-	},
-
-	removeFromFavorite: function(event) {
-		console.log('removeFromFavorite');
-
-	},
-
-
-	addToFavorite: function(event) {
-		console.log('addToFavorite');
+				.click($.proxy(this.onAddFavoriteIconClick, this));
 	},
 
 
 
+	/**
+	 * Handle remove click
+	 *
+	 * @param	{Object}	event
+	 */
+	onRemoveFavoriteIconClick: function(event) {
+		var institutionBox 		= $(event.target).parents('.institutionBox').get(0),
+			institutionCode		= institutionBox.id.split('-')[3];
+
+		this.updateFavorite(institutionCode, 'delete');
+	},
+
+
+
+	/**
+	 * Handle add click
+	 *
+	 * @param	{Object}	event
+	 */
+	onAddFavoriteIconClick: function(event) {
+		var institutionBox 		= $(event.target).parents('.institutionBox').get(0),
+			institutionCode		= institutionBox.id.split('-')[3];
+
+		this.updateFavorite(institutionCode, 'add');
+	},
+
+
+
+	/**
+	 * Send favorite update request (add or delete) for institution
+	 *
+	 * @param	{String}	institutionCode
+	 * @param	{String}	action
+	 * @param	{Function}	[callback]
+	 */
+	updateFavorite: function(institutionCode, action, callback) {
+		var url	= this.baseUrl + '/' + action,
+			data= {
+				institution: institutionCode
+			},
+			success	= function(response){
+						if(callback) {
+							callback(institutionCode, action, response);
+						} else {
+							$('body').mask(vufindString.favoriteReload);
+							location.reload();
+						}
+					};
+
+		$.post(url, data, success);
+	},
+
+
+
+	/**
+	 * Get codes of favorite institutions (in favorite box)
+	 *
+	 * @returns {String[]}
+	 */
 	getFavoriteInstitutionCodes: function() {
 		var favoriteTogglers = $('#holdings-favorite').find('.institutionToggler');
 
-		return this.extractInstitutionCodes(favoriteTogglers);
-	},
-
-	extractInstitutionCodes: function(items) {
-		return $.map(items, function(node, index){
+		return $.map(favoriteTogglers, function(node, index){
 			return $(node).attr('id').split('-').pop();
 		});
 	}
