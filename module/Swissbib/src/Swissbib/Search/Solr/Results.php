3a88b3c7ef3ff305efa7311427282104b2817b48
Fabian Erni <ferni@snowflake.ch>
2013-07-04 11:08:03 +0200

    * Add user institution favorites as normal facets on top of facet list * Removed special handling and template for user institution facets * Added translation for facet titles * Sort facet query items by count * Fixed translation of filters

--- module/Swissbib/src/Swissbib/Search/Solr/Results.php
+++ module/Swissbib/src/Swissbib/Search/Solr/Results.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Swissbib\Search\Solr;
 
 /**
  * swissbib extended Results type for the Solr target
@@ -31,90 +31,155 @@
  * @link     http://www.swissbib.org
  */
 
-namespace Swissbib\Search\Solr;
-
 use VuFind\Search\Solr\Results as VFSolrResults;
 use VuFindSearch\Query\AbstractQuery;
 use VuFindSearch\Query\QueryGroup;
-//use VuFindSearch\Query\Query;
 use VuFindSearch\ParamBag;
-//use VuFindSearch\Backend\Solr\Response\Json\Spellcheck;
-
-
 
 /**
  * Class to extend the core VF2 SOLR functionality related to Solr Results
  */
+class Results extends VFSolrResults
+{
 
-class Results extends VFSolrResults {
+	/**
+	 * Create backend parameters
+	 * Add facet queries for user institutions
+	 *
+	 * @param	AbstractQuery	$query
+	 * @param	Params        	$params
+	 * @return	ParamBag
+	 */
+	protected function createBackendParameters(AbstractQuery $query, Params $params)
+	{
+		$backendParams = parent::createBackendParameters($query, $params);
 
-    public function __construct(Params $params)
-    {
-        parent::__construct($params);
-    }
+		//with SOLR 4.3 AND is no longer the default parameter
+		$backendParams->add("q.op", "AND");
 
+		$backendParams = $this->addUserInstitutions($backendParams);
 
-    protected function createBackendParameters(AbstractQuery $query, Params $params)  {
-
-        $paramBag = parent::createBackendParameters($query,$params);
-
-        //with SOLR 4.3 AND is no longer the default parameter
-        $paramBag->add("q.op","AND");
-
-
-        //create query parameters for favorites
-        $favoriteInstitutions =  $this->getParams()->getUserFavoritesInstitutions();
-        if (sizeof( $favoriteInstitutions > 0 )) {
-
-            //facet parameter has to be true in case it's false
-            $paramBag->remove("facet");
-            $paramBag->add("facet","true");
-
-            foreach ($favoriteInstitutions as $instititution) {
-
-                $paramBag->add("facet.query","institution:" . $instititution);
-                $paramBag->add("facet.query","institution:" . $instititution);
-
-            }
-
-            foreach ($favoriteInstitutions as $instititution) {
-
-                $paramBag->add("bq","institution:" . $instititution . "^5000");
-
-            }
-
-        }
-
-        return $paramBag;
-
-    }
+		return $backendParams;
+	}
 
 
-    public function getFavoritesFacets() {
 
-        $favoritesWithNonZero = array();
-        $iterator  = $this->responseFacets->getQueryFacets()->getIterator();;
+	/**
+	 * Add user institutions as facet queries to backend params
+	 *
+	 * @param	ParamBag	$backendParams
+	 * @return	ParamBag
+	 */
+	protected function addUserInstitutions(ParamBag $backendParams)
+	{
+		/** @var String[] $favoriteInstitutions */
+		$favoriteInstitutions = $this->getParams()->getUserFavoritesInstitutions();
 
-        while($iterator->valid()) {
+		if (sizeof($favoriteInstitutions > 0)) {
+				//facet parameter has to be true in case it's false
+			$backendParams->set("facet", "true");
 
-            if (strpos($iterator->key(), "institution:" ) === 0 && $iterator->current() > 0) {
+			foreach ($favoriteInstitutions as $institutionCode) {
+				$backendParams->add("facet.query", "institution:" . $institutionCode);
+				$backendParams->add("bq", "institution:" . $institutionCode . "^5000");
+			}
+		}
 
-                $tParts = explode(":",$iterator->key());
+		return $backendParams;
+	}
 
-                $facetItem = array(
-                    "value" => $tParts[1],
-                    "displayText" => $tParts[1], //todo: how to translate the value??
-                    "count" => $iterator->current(),
-                    "isApplied" => false //todo: lookup used filters to decide if this item should be applied
-                );
 
-                $favoritesWithNonZero [] = $facetItem;
-            }
 
-            $iterator->next();
-        }
+	/**
+	 * Get facet queries from result
+	 * Data is extracted
+	 * Format: {field, value, count, name}
+	 *
+	 * @param	Boolean		$onlyNonZero
+	 * @return	Array[]
+	 */
+	protected function getResultQueryFacets($onlyNonZero = false)
+	{
+		/** @var \ArrayObject $queryFacets */
+		$queryFacets = $this->responseFacets->getQueryFacets();
+		$facets		= array();
 
-        return $favoritesWithNonZero;
-    }
+		foreach ($queryFacets as $facetName => $queryCount) {
+			list($fieldName,$filterValue) = explode(':', $facetName, 2);
 
+			if (!$onlyNonZero || $queryCount > 0) {
+				$facets[] = array(
+					'field'	=> $fieldName,
+					'value'	=> $filterValue,
+					'count'	=> $queryCount,
+					'name'	=> $facetName
+				);
+			}
+		}
+
+		return $facets;
+	}
+
+
+
+	/**
+	 * Get special facets
+	 * - User favorite institutions
+	 *
+	 * @return	Array[]
+	 */
+	public function getSpecialFacets()
+	{
+		$queryFacets	= $this->getResultQueryFacets(true);
+		$facetListItems	= array();
+
+		foreach ($queryFacets as $queryFacet) {
+			if ($queryFacet['field'] === 'institution') {
+				$sortKey	= $queryFacet['count'] . '_' . $queryFacet['value']; // Sortable but unique key
+
+				$facetListItems[$sortKey] = array(
+					'value'			=> $queryFacet['value'],
+					'displayText'	=> $queryFacet['value'],
+					'count'			=> $queryFacet['count'],
+					'isApplied'		=> $this->getParams()->hasFilter($queryFacet['name'])
+				);
+			}
+		}
+
+		if (empty($facetListItems)) {
+			return array();
+		}
+
+			// Sort by count (which is the key)
+		ksort($facetListItems);
+		$facetListItems = array_values($facetListItems);
+
+		return array(
+			'userInstitutions' => array(
+				'label'	=> 'mylibraries',
+				'field'	=> 'institution',
+				'list'	=> $facetListItems
+			)
+		);
+	}
+
+
+
+	/**
+	 * Get facet list
+	 * Add institution query facets on top of the list
+	 *
+	 * @param	Array|Null		$filter
+	 * @return	Array[]
+	 */
+	public function getFacetList($filter = null)
+	{
+		$facetList 				= parent::getFacetList($filter);
+		$userInstitutionFacets	= $this->getSpecialFacets();
+
+			// Prepend special facets
+		$facetList = $userInstitutionFacets + $facetList;
+
+		return $facetList;
+	}
 }
