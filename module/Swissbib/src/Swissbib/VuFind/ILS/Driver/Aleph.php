f33d6c6a1bcf83697a1f521be60e0807f275b863
Oliver Schihin <oliver.schihin@unibas.ch>
2013-08-05 16:34:05 +0200

    * removed obsolete function doXRequest(), used for testing other port * uncommented config for port

--- module/Swissbib/src/Swissbib/VuFind/ILS/Driver/Aleph.php
+++ module/Swissbib/src/Swissbib/VuFind/ILS/Driver/Aleph.php
@@ -368,46 +368,6 @@ class Aleph extends VuFindDriver
 	}
 
 
-
-	/**
-	 * Fix xserver port problem
-	 *
-	 * @param    String         $op
-	 * @param    Array          $params
-	 * @param    Boolean        $auth
-	 * @return SimpleXMLElement
-	 * @throws ILSException
-	 * @throws \Exception
-	 */
-	protected function doXRequest($op, $params, $auth = false)
-	{
-		if (isset($this->config['xServer']['port'])) {
-			$port = $this->config['xServer']['port'];
-			$auth = isset($this->config['xServer']['auth']) ? !!$this->config['xServer']['auth'] : $auth;
-
-			try {
-				$oldHost = $this->host;
-				$this->host .= ':' . $port;
-
-				$returnValue = parent::doXRequest($op, $params, $auth);
-
-				$this->host = $oldHost;
-
-				return $returnValue;
-			} catch (\Exception $e) {
-				// Reset host to leave it clear
-				$this->host = $oldHost;
-				// Go on with exception
-				throw $e;
-			}
-		} else {
-			// Normal handling
-			return parent::doXRequest($op, $params, $auth);
-		}
-	}
-
-
-
 	/**
 	 * Perform a RESTful DLF request.
 	 *
