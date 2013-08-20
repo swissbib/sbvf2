4035ef5a0f54a9bc6c5bb6ce0fb665dd1e8ac029
Andres von Arx <andres.vonarx@unibas.ch>
2013-08-20 15:19:25 +0200

    Tab 2: reworked checkbox facets default filters are now: with holdings/fulltext and  without newspaper articles

--- module/Swissbib/src/Swissbib/VuFind/Search/Summon/Results.php
+++ module/Swissbib/src/Swissbib/VuFind/Search/Summon/Results.php
@@ -3,27 +3,74 @@
 
 namespace Swissbib\Vufind\Search\Summon;
 
+use SerialsSolutions_Summon_Query as SummonQuery,
+    VuFind\Exception\RecordMissing as RecordMissingException,
+    VuFind\Search\Base\Results as BaseResults,
+    VuFind\Solr\Utils as SolrUtils,
+    VuFindSearch\ParamBag;
+
 use VuFind\Search\Summon\Results as VFSummonResults;
 
 
 class Results extends VFSummonResults
 {
-
-
     /**
-     * Create search backend parameters for advanced features.
+     * Set up filters based on VuFind settings.
      *
-     * @param Params $params Search parameters
+     * @param ParamBag $params     Parameter collection to update
+     * @param array    $filterList Filter settings
      *
-     * @return ParamBag
+     * @return void
      */
-    protected function createBackendParameters(Params $params)
+    public function createBackendFilterParameters(ParamBag $params, $filterList)
     {
+        // flag our non-Standard checkbox filters:
+        $foundIncludeNewspapers = false;        # includeNewspapers
+        $foundIncludeWithoutFulltext = false;   # includeWithoutFulltext
 
-        parent::createBackendParameters($params);
+        // Which filters should be applied to our query?
+        if (!empty($filterList)) {
+            // Loop through all filters and add appropriate values to request:
+            foreach ($filterList as $filterArray) {
+                foreach ($filterArray as $filt) {
+                    $safeValue = SummonQuery::escapeParam($filt['value']);
+                    if ($filt['field'] == 'holdingsOnly') {
+                        // Special case -- "holdings only" is a separate parameter from
+                        // other facets.
+                        $params->set('holdings', strtolower(trim($safeValue)) == 'true');
+                    } else if ($filt['field'] == 'excludeNewspapers') {
+                        // support a checkbox for excluding newspapers:
+                        // this is now the default behaviour.
+                    } else if ($filt['field'] == 'includeNewspapers') {
+                        // explicitly include newspaper articles
+                        $foundIncludeNewspapers = true;
+                    } else if ($range = SolrUtils::parseRange($filt['value'])) {
+                        // Special case -- range query (translate [x TO y] syntax):
+                        $from = SummonQuery::escapeParam($range['from']);
+                        $to = SummonQuery::escapeParam($range['to']);
+                        $params->add('rangeFilters', "{$filt['field']},{$from}:{$to}");
+                    } else if ($filt['field'] == 'includeWithoutFulltext') {
+                        $foundIncludeWithoutFulltext = true;
+                    } else {
+                        // Standard case:
+                        $params->add('filters', "{$filt['field']},{$safeValue}");
+                    }
+                }
+            }
+        }
+        // special cases (apply also when filter list is empty)
+        // newspaper articles
+        if ( ! $foundIncludeNewspapers ) {
+            // this actually means: do *not* show newspaper articles
+            $params->add('filters', "ContentType,Newspaper Article,true");
+        }
+        // combined facet "with holdings/with fulltext"
+        if ( !$foundIncludeWithoutFulltext ) {
+            $params->set('holdings', true);
+            $params->add('filters',  'IsFullText,true');
 
-
-   }
-
-
+        } else {
+            $params->set('holdings', false);
+        }
+    }
 }
