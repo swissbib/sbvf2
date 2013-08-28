e93c1387e329b5f7f90b3d1947888bcfdba006eb
Andres von Arx <andres.vonarx@unibas.ch>
2013-08-28 09:09:28 +0200

    bugfix in Swissbib/View/Helper/YearFormatterSummon

--- module/Swissbib/src/Swissbib/View/Helper/YearFormatterSummon.php
+++ module/Swissbib/src/Swissbib/View/Helper/YearFormatterSummon.php
@@ -19,7 +19,6 @@ class YearFormatterSummon extends AbstractHelper
 	 * @param   Array   $publicationDate
 	 * @return  String
 	 */
-
 	public function __invoke($publicationDate)
 	{
 		if(is_array($publicationDate)) {
