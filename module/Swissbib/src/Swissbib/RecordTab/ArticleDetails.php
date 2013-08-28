07bc012b607086ed9c77c9371de6cc6074232c8a
Andres von Arx <andres.vonarx@unibas.ch>
2013-08-28 14:33:30 +0200

    List view and record view of Summon target

---
+++ module/Swissbib/src/Swissbib/RecordTab/ArticleDetails.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *  details tab for target 'Articles & more'
+ */
+
+namespace Swissbib\RecordTab;
+
+use \VuFind\RecordTab\AbstractBase;
+
+class ArticleDetails extends AbstractBase
+{
+	/**
+	 * Get the on-screen description for this tab.
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return 'Description';
+	}
+}
