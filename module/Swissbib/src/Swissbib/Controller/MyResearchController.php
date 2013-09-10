57f7141887aece2ec958dc5f80e9d1c8b16b8041
Guenter Hipler <guenter.hipler@unibas.ch>
2013-09-10 11:31:21 +0200

    integration of guest account in login form
    todo: improve CSS and compile help and information links and content

--- module/Swissbib/src/Swissbib/Controller/MyResearchController.php
+++ module/Swissbib/src/Swissbib/Controller/MyResearchController.php
@@ -227,4 +227,49 @@ class MyResearchController extends VuFindMyResearchController
 
 		return $layout;
 	}
+
+
+
+    /**
+     * Convenience method to get a session initiator URL. Returns false if not
+     * applicable.
+     * what does "not applicable" mean:
+     * for me (GH) it makes no sense to create a session initiator instance in case we are within the normal workflow of the application
+     * (no authentication procedure in conjunction with shibboleth authentication took place)
+     * at the moment I compare the domain strings to decide if we should create a session initiator because an authentication with shibboleth tool place
+     * another possibilty might be to test the Sibboleth.sso/Session response
+     * at the moment we have to issues:
+     * a) why redirect prefix in apache session variables?
+     * b) access to the shibboleth session variables is only possible immediately after shibboleth authentication process - why?
+     * question are pending at switch
+     *
+     * @return string|bool
+     */
+    protected function getSessionInitiator()
+    {
+
+        //$bag  = array();
+       //foreach($this->getRequest()->getServer() as $key => $value) {
+        //    $bag[$key] = $value;
+        //}
+
+        $uri = $this->getRequest()->getUri();
+        $base = sprintf('%s://%s', $uri->getScheme(), $uri->getHost());
+        $baseEscaped =  str_replace("/","\/",$base);
+
+        $tReferrer = $this->getRequest()->getServer()->get('HTTP_REFERER');
+        if (preg_match("/$baseEscaped/",$this->getRequest()->getServer()->get('HTTP_REFERER')) == 0) {
+
+            $url = $this->getServerUrl('myresearch-home');
+            return $this->getAuthManager()->getSessionInitiator($url);
+
+        } else {
+
+            return false;
+        }
+
+
+
+    }
+
 }
