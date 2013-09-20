02d0c67a2703bc0d6627d033a089f3badd00f632
mmaechler <mmaechler@snowflake.ch>
2013-09-20 09:47:15 +0200

    * #132 Added holding-holding, Updated holding-item

--- module/Swissbib/src/Swissbib/RecordDriver/Helper/Holdings.php
+++ module/Swissbib/src/Swissbib/RecordDriver/Helper/Holdings.php
@@ -562,6 +562,9 @@ class Holdings
 			$holding['backlink'] = $this->getBackLink($holding['network'], strtoupper($holding['institution']), $holding);
 //		}
 
+		$bibInfoLink = $this->getBibInfoLink(strtolower($holding['institution']));
+		$holding['institutionUrl'] = $bibInfoLink['url'];
+
 		return $holding;
 	}
 
