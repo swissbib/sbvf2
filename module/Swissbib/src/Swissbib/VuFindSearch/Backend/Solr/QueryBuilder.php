70e736101f7b4348ad92ac146c895392f69a081b
Guenter Hipler <guenter.hipler@unibas.ch>
2013-09-20 17:36:22 +0200

    dedicated QueryBuilder for swissbib
    fixes issue
    https://github.com/swissbib/sbvf2/issues/125
    (see comments for more background and information)

---
+++ module/Swissbib/src/Swissbib/VuFindSearch/Backend/Solr/QueryBuilder.php
@@ -0,0 +1,98 @@
+<?php
+ 
+ /**
+ * [...description of the type ...]
+ *
+ * PHP version 5
+ *
+ * Copyright (C) project swissbib, University Library Basel, Switzerland
+ * http://www.swissbib.org  / http://www.swissbib.ch / http://www.ub.unibas.ch
+ *
+ * Date: 9/20/13
+ * Time: 12:51 PM
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category swissbib_VuFind2
+ * @package  [...package name...]
+ * @author   Guenter Hipler  <guenter.hipler@unibas.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.swissbib.org
+ */
+
+
+namespace Swissbib\VuFindSearch\Backend\Solr;
+
+
+use VuFindSearch\Backend\Solr\QueryBuilder as VFBuilder;
+
+
+class QueryBuilder extends VFBuilder {
+
+
+    protected $disMaxSearchFields = array();
+
+    public function __construct(array $specs = array())
+    {
+        parent::__construct($specs);
+
+        if (array_key_exists("allfields",$this->specs) && array_key_exists("DismaxFields",  $this->specs["allfields"]->toArray())) {
+            $this->disMaxSearchFields = $this->specs["allfields"]->toArray()["DismaxFields"];
+        }
+
+        $this->disMaxSearchFields = array_map(function($item) {
+
+            if (strpos($item,"^") > 0) {
+                return substr($item,0,strpos($item,"^")  );
+
+            } else {
+                return $item;
+            }
+
+
+        }, $this->disMaxSearchFields );
+        //this search field isn't defined in searchspec
+        $this->disMaxSearchFields[] = "hierarchy_parent_id";
+
+
+
+
+    }
+
+
+    protected function prepareForLuceneSyntax($input) {
+
+        $alreadyPrepared = parent::prepareForLuceneSyntax($input);
+
+        preg_match_all("/(?P<name>\w+?:|[ ]:)/",$input,$matches);
+
+        if (count($matches["name"] > 0) ) {
+
+            foreach ($matches["name"] as $fieldNameWithColon) {
+
+                $fieldNameNoColon = substr($fieldNameWithColon, 0, strpos($fieldNameWithColon,":"));
+
+                if (!in_array($fieldNameNoColon,$this->disMaxSearchFields)) {
+                    $alreadyPrepared = str_replace($fieldNameWithColon,$fieldNameNoColon . " ",$alreadyPrepared);
+                }
+            };
+
+
+        }
+
+        return $alreadyPrepared;
+
+    }
+
+
+}
