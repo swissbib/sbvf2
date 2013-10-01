df67c810d416fc7f49cee1bafb71e208782a135e
Guenter Hipler <guenter.hipler@unibas.ch>
2013-10-01 17:49:51 +0200

    no popup dialogs for Shibboleth login
    using a Shibboleth login means to leave the application window and to use the login page of an external IDP
    this makes it very complex (if not impossible) to capture the whole workflow within a dialog popup box
    (as it is done for more easier Library system authentication where VuFind is always in control of the workflow)
    
    Therefor I need a possibility to send the user to the default login page even this not so good related to usability aspects
    
    This commit implements the mechanism to control the different workflows depending on the different Authentication methods

--- module/Swissbib/src/Swissbib/VuFind/Auth/Shibboleth.php
+++ module/Swissbib/src/Swissbib/VuFind/Auth/Shibboleth.php
@@ -7,6 +7,7 @@ namespace Swissbib\VuFind\Auth;
 
 
 use VuFind\Auth\Shibboleth as VuFindShib;
+use VuFind\Exception\Auth as AuthException;
 
 
 
@@ -21,7 +22,15 @@ class Shibboleth extends  VuFindShib{
     public function isExpired()
     {
         //return parent::isExpired();
+        //todo: we still have to solve some issues:
+        //-- can we solve the REDIRECT_ proefix issue in Shibboleth Server variables? If yes, isExpired
+        //could be implemented by looking up server variables
+
+        //or another strategy might be to request the Shibbiloth sessionID
+        //it all depends on the next evaluation (together with SWITCH)
         return false;
     }
+
+
 
 }
