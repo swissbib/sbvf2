95f1369cf208ca7cba2f3afb6288457195548c16
mmaechler <mmaechler@snowflake.ch>
2013-10-18 16:39:37 +0200

    #50 Override Vufind XSLT-Processor

---
+++ module/Swissbib/src/Swissbib/XSLT/Processor.php
@@ -0,0 +1,37 @@
+<?php
+namespace Swissbib\XSLT;
+
+use \VuFind\XSLT\Processor as VFProcessor;
+use \DOMDocument;
+use \XSLTProcessor;
+
+
+class Processor extends VFProcessor
+{
+	/**
+	 * Perform an XSLT transformation and return the results.
+	 *
+	 * @param string $xslt   Name of stylesheet (in application/xsl directory)
+	 * @param string $xml    XML to transform with stylesheet
+	 * @param array $params Associative array of XSLT parameters
+	 *
+	 * @return string      Transformed XML
+	 */
+	public static function process($xslt, $xml, $params = array())
+	{
+		if ($xslt != 'record-marc.xsl') return parent::process($xslt, $xml, $params);
+
+		$style = new DOMDocument();
+		$style->load(APPLICATION_PATH . '/module/Swissbib/xsl/' . $xslt);
+		$xsl = new XSLTProcessor();
+		$xsl->importStyleSheet($style);
+		$doc = new DOMDocument();
+		if ($doc->loadXML($xml)) {
+			foreach ($params as $key => $value) {
+				$xsl->setParameter('', $key, $value);
+			}
+			return $xsl->transformToXML($doc);
+		}
+		return '';
+	}
+}
