efe437043635a36cb7393697b2b6781379473a49
mmaechler <mmaechler@snowflake.ch>
2013-10-25 17:17:52 +0200

    #50 Bugfix ordering and vtls

--- module/Swissbib/src/Swissbib/XSLT/MARCFormatter.php
+++ module/Swissbib/src/Swissbib/XSLT/MARCFormatter.php
@@ -8,23 +8,33 @@ class MARCFormatter
      * @var array
      */
     protected static $institutionURLs = array(
-        "NEBIS"  => "http://opac.nebis.ch/F/?local_base=EBI01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
-        "IDSBB"  => "http://aleph.unibas.ch/F/?local_base=DSV01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
+        "NEBIS" => "http://opac.nebis.ch/F/?local_base=EBI01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
+        "IDSBB" => "http://aleph.unibas.ch/F/?local_base=DSV01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
         "IDSSG2" => "http://aleph.unisg.ch/F?local_base=HSB02&con_lng=GER&func=direct&doc_number=%s",
         "IDSSG" => "http://aleph.unisg.ch/F?local_base=HSB01&con_lng=GER&func=direct&doc_number=%s",
-        "SBT"    => "http://aleph.sbt.ti.ch/F?local_base=SBT01&con_lng=ITA&func=find-b&find_code=SYS&request=%s",
-        "SNL"    => "http://opac.admin.ch/cgi-bin/gw/chameleon?lng=de&inst=consortium&search=KEYWORD&function=CARDSCR&t1=%s&u1=12101",
+        "SBT" => "http://aleph.sbt.ti.ch/F?local_base=SBT01&con_lng=ITA&func=find-b&find_code=SYS&request=%s",
+        "SNL" => "http://opac.admin.ch/cgi-bin/gw/chameleon?lng=de&inst=consortium&search=KEYWORD&function=CARDSCR&t1=%s&u1=12101",
         "RERO" => "http://opac.rero.ch/gateway?beginsrch=1&lng=de&inst=consortium&search=KEYWORD&function=CARDSCR&t1=%s&u1=12",
-        "IDSLU"  => "http://ilu.zhbluzern.ch/F/?local_base=ILU01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
-        "OCoLC"  => "http://www.worldcat.org/search?q=no%3A%s",
-        "CCSA"   => "http://opac.admin.ch/cgi-bin/gw/chameleon?skin=affiches&lng=de&inst=consortium&search=KEYWORD&function=INITREQ&t1=%s&u1=12101",
+        "IDSLU" => "http://ilu.zhbluzern.ch/F/?local_base=ILU01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
+        "OCoLC" => "http://www.worldcat.org/search?q=no%3A%s",
+        "CCSA" => "http://opac.admin.ch/cgi-bin/gw/chameleon?skin=affiches&lng=de&inst=consortium&search=KEYWORD&function=INITREQ&t1=%s&u1=12101",
         "CHARCH" => "http://www.helveticarchives.ch/detail.aspx?ID=%s",
-        "BGR"    => "http://aleph.gr.ch/F/?local_base=BGR01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
-        "ABN"    => "http://aleph.ag.ch/F/?local_base=ABN01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
-        "SGBN"   => "http://aleph.sg.ch/F/?local_base=SGB01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
-        "ALEX"   => "http://opac.admin.ch/cgi-bin/gwalex/chameleon?lng=de&inst=consortium&skin=portal&search=KEYWORD&function=CARDSCR&t1=%s&u1=12101"
+        "BGR" => "http://aleph.gr.ch/F/?local_base=BGR01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
+        "ABN" => "http://aleph.ag.ch/F/?local_base=ABN01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
+        "SGBN" => "http://aleph.sg.ch/F/?local_base=SGB01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
+        "ALEX" => "http://opac.admin.ch/cgi-bin/gwalex/chameleon?lng=de&inst=consortium&skin=portal&search=KEYWORD&function=CARDSCR&t1=%s&u1=12101"
     );
 
+
+
+    /**
+     * @var array
+     */
+    protected static $trimVTLS = array(
+        "CCSA",
+        "SNL",
+        "ALEX"
+    );
 
 
     /**
@@ -35,12 +45,15 @@ class MARCFormatter
     public static function compileSubfield(array $domArray)
     {
         $domNode = $domArray[0];
+        if ($domNode->parentNode->getAttribute('tag') != '035') return $domNode; //return before trying to find institution
+
         $nodeValue = trim($domNode->textContent);
         $institution = self::getInstitutionFromNodeText($nodeValue);
 
-        if ($domNode->parentNode->getAttribute('tag') != '035' || $domNode->getAttribute('code') != 'a' || empty($institution)) {
+        if ($domNode->getAttribute('code') != 'a' || empty($institution)) {
             return $domNode;
         } else {
+            if (in_array($institution,self::$trimVTLS)) $nodeValue = str_replace('vtls', '',$nodeValue);
             $request = substr($nodeValue, strlen($institution) + 2);
             $url = str_replace('%s', $request, self::$institutionURLs[$institution]);
 
@@ -57,9 +70,14 @@ class MARCFormatter
      */
     protected static function getInstitutionFromNodeText($nodeText)
     {
-        foreach (self::$institutionURLs as $key => $value) {
-            if (strpos($nodeText, $key) !== false) {
-                return $key;
+        preg_match('/\(([a-zA-Z0-9]+)\)/', $nodeText, $matches);
+        $match = $matches[1];
+
+        if (!empty($match)) {
+            foreach (self::$institutionURLs as $key => $value) {
+                if ($match === $key) {
+                    return $key;
+                }
             }
         }
 
