31411a879a67f39e989c4845f8a703104a5eff58
mmaechler <mmaechler@snowflake.ch>
2013-10-30 15:24:04 +0100

    #255 overwrite lightbox, close action

---
+++ themes/swissbib/js/blueprint/lightbox.js
@@ -0,0 +1,81 @@
+function getLightbox(module, action, id, lookfor, message, followupModule, followupAction, followupId, postParams) {
+	// Optional parameters
+	if (followupModule === undefined) {followupModule = '';}
+	if (followupAction === undefined) {followupAction = '';}
+	if (followupId     === undefined) {followupId     = '';}
+
+	var params = {
+		method: 'getLightbox',
+		lightbox: 'true',
+		submodule: module,
+		subaction: action,
+		id: id,
+		lookfor: lookfor,
+		message: message,
+		followupModule: followupModule,
+		followupAction: followupAction,
+		followupId: followupId
+	};
+
+	// create a new modal dialog
+	var $dialog = $('<div id="modalDialog"><div class="dialogLoading">&nbsp;</div></div>')
+		.load(path + '/AJAX/JSON?' + $.param(params), postParams)
+		.dialog({
+			modal: true,
+			autoOpen: false,
+			closeOnEscape: true,
+			width: 600,
+			height: "auto",
+			close: function () {
+				// check if the dialog was successful, if so, load the followup action
+				if (__dialogHandle.processFollowup && __dialogHandle.followupModule
+					&& __dialogHandle.followupAction) {
+					$(this).remove();
+					getLightbox(__dialogHandle.followupModule, __dialogHandle.followupAction,
+						__dialogHandle.recordId, null, message, null, null, null, postParams);
+				}
+				$(this).remove();
+			}
+		});
+
+	// save information about this dialog so we can get it later for followup processing
+	__dialogHandle.dialog = $dialog;
+	__dialogHandle.processFollowup = false;
+	__dialogHandle.followupModule = followupModule;
+	__dialogHandle.followupAction = followupAction;
+	__dialogHandle.recordId = followupId == '' ? id : followupId;
+	__dialogHandle.postParams = postParams;
+
+	// done
+	return $dialog.dialog('open');
+}
+
+function registerCloseButton() {
+	jQuery("#modalDialog .close").click(function(event){
+		event.preventDefault();
+		jQuery("#modalDialog").dialog("close");
+	});
+}
+
+/**
+ * This is called by the lightbox when it
+ * finished loading the dialog content from the server
+ * to register the form in the dialog for ajax submission.
+ */
+function lightboxDocumentReady() {
+	registerAjaxLogin();
+	registerAjaxCart();
+	registerAjaxCartExport();
+	registerAjaxSaveRecord();
+	registerAjaxListEdit();
+	registerAjaxEmailRecord();
+	registerAjaxSMSRecord();
+	registerAjaxTagRecord();
+	registerAjaxEmailSearch();
+	registerAjaxBulkSave();
+	registerAjaxBulkEmail();
+	registerAjaxBulkExport();
+	registerAjaxBulkDelete();
+	registerCloseButton();
+	$('.mainFocus').focus();
+}
