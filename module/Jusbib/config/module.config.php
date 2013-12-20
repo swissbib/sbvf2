7db5c479322e117977e3ab8098b57883fe4b3590
mmaechler <mmaechler@snowflake.ch>
2013-12-20 16:37:09 +0100

    #409 Implemented Tabs in Advanced Search Part 1

--- module/Jusbib/config/module.config.php
+++ module/Jusbib/config/module.config.php
@@ -1,49 +1,58 @@
 <?php
 namespace Jusbib\Module\Config;
 
-use Zend\Config\Config;
-use Zend\I18n\Translator\Translator;
-
-use Swissbib\TargetsProxy\TargetsProxy;
-use Swissbib\TargetsProxy\IpMatcher;
-use Swissbib\TargetsProxy\UrlMatcher;
-use Swissbib\Theme\Theme;
-use Swissbib\Libadmin\Importer as LibadminImporter;
-use Swissbib\RecordDriver\Helper\Holdings as HoldingsHelper;
-use Swissbib\View\Helper\InstitutionSorter;
-use Swissbib\Tab40Import\Importer as Tab40Importer;
-use Swissbib\RecordDriver\Helper\LocationMap;
-use Swissbib\RecordDriver\Missing as RecordDriverMissing;
-use Swissbib\RecordDriver\Summon;
-use Swissbib\RecordDriver\WorldCat;
-use Swissbib\RecordDriver\Helper\EbooksOnDemand;
-use Swissbib\RecordDriver\Helper\Availability;
-use Swissbib\Helper\BibCode;
-use Swissbib\Favorites\DataSource as FavoritesDataSource;
-use Swissbib\Favorites\Manager as FavoritesManager;
-use Swissbib\Favorites\Manager;
-use Swissbib\View\Helper\ExtractFavoriteInstitutionsForHoldings;
-use Swissbib\View\Helper\IsFavoriteInstitution;
-use Swissbib\VuFind\Search\Helper\ExtendedSolrFactoryHelper;
-use Swissbib\View\Helper\QrCode as QrCodeViewHelper;
-use Swissbib\Highlight\SolrConfigurator as HighlightSolrConfigurator;
-use Swissbib\VuFind\Hierarchy\TreeDataSource\Solr as TreeDataSourceSolr;
-use Swissbib\Log\Logger as SwissbibLogger;
-use Swissbib\View\Helper\DomainURL;
+use Jusbib\Theme\Theme;
 
 return array(
-
+    'router' => array(
+        'routes' => array(
+            'search-advancedClassification' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/Search/AdvancedClassification',
+                    'defaults' => array(
+                        'controller' => 'search',
+                        'action'     => 'advancedClassification'
+                    )
+                )
+            )
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'search' => 'Jusbib\Controller\SearchController',
+        )
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Jusbib\Theme\Theme' => function () {
+                    return new Theme();
+                }
+        )
+    ),
     'swissbib' => array(
-		// Search result tabs
-		'resultTabs' => array(
-				// Active tabs for a theme
-			'themes' => array(
+        'resultTabs' => array(
+            'themes' => array(
                 'jusbib' => array(
                     'swissbib'
                 )
-
-
-			)
-		)
-	)
+            )
+        )
+    ),
+    'jusbib' => array(
+        'adv_tabs' => array(
+            'swissbib'       => array(
+                'searchClassId' => 'Solr', // VuFind searchClassId
+                'label'         => 'Advanced Search', // Label
+                'type'          => 'swissbibsolr', // Key for custom templates
+                'advSearch'     => 'search-advanced'
+            ),
+            'classification' => array(
+                'searchClassId' => 'Solr',
+                'label'         => 'classification_tree',
+                'type'          => 'swissbibsolr',
+                'advSearch'     => 'search-advancedClassification'
+            )
+        )
+    )
 );
